c73de8fc51463061a50f7a973771719caff8b179
Ethan Koran <ethankoran@ethans-mbp.dhcp.nd.edu>
2024-12-05 23:17:06 -0500

    more updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,12 @@
 
 import { FloatingDock } from '@/components/FloatingDock'
 import { useEffect } from 'react'
+import React from "react";
 import Typed from 'typed.js'
 import Link from 'next/link'
+import Image from "next/image";
 import { FloatingNav } from "@/components/FloatingNavBar";
+import { Carousel, Card } from "@/components/CardCarousel";
 
 export default function Home() {
   const navItems = [
@@ -54,6 +57,60 @@ export default function Home() {
     }
   ];
 
+  const DummyContent = () => {
+    return ( <div> Hello </div> ) 
+  }
+
+  const experiences = [
+    {
+      category: "2022-2026",
+      title: "My time at Notre Dame",
+      src: "/images/dome.jpg",
+      content: <DummyContent />,
+    },
+    {
+      category: "August 2024 - Present",
+      title: "IrishSat NASA CubeSat Launch Initiative",
+      src: "/images/satellite.jpg",
+      content: <DummyContent />,
+    },
+    {
+      category: "April 2024 - Present",
+      title: "Sports Biomechanics Research Lab",
+      src: "/images/softball.jpeg",
+      content: <DummyContent />,
+    },
+    {
+      category: "May 2023 - July 2023",
+      title: "ND Study Abroad - London, England",
+      src: "/images/london.jpeg",
+      content: <DummyContent />,
+    },
+    {
+      category: "July 2024",
+      title: "Alumni Family Volunteer Camp",
+      src: "/images/me.jpg",
+      content: <DummyContent />,
+    },
+    {
+      category: "September 2023 - May 2024",
+      title: "Computer Science for Good",
+      src: "/images/me.jpg",
+      content: <DummyContent />,
+    },
+    {
+      category: "March 2015 - September 2021",
+      title: "Boy Scouts of America",
+      src: "/images/me.jpg",
+      content: <DummyContent />,
+    }
+  ]
+
+  const cards = experiences.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
+
   useEffect(() => {
     const iamoptions = {
       strings: ["Student", "Web Designer", "Software Developer", "Musician", "World Traveler"],
@@ -75,16 +132,16 @@ export default function Home() {
   return (
     <main className="flex">
       <FloatingNav navItems={navItems} className="" />
-      <div className="bg-gray-100 text-gray-900">
+      <div className="bg-gray-100 text-gray-900 z-1">
         {/* Home Section */}
         <section 
           id="home" 
-          className="min-h-screen flex flex-col items-center justify-center bg-white bg-cover bg-top bg-no-repeat relative text-center p-20"
-          style={{ backgroundImage: 'url(/images/me.jpg)' }}>
+          className="min-h-screen flex flex-col items-center justify-center bg-white bg-no-repeat bg-cover bg-top relative text-center p-20 bg-[url('/images/me.jpg')] md:bg-[url('/images/me_cropped.jpeg')]"
+          >
           <div className="absolute inset-0 bg-white bg-opacity-60 z-0"></div> 
-          <div className="relative z-10 mt-24">
-            <h1 className="text-4xl sm:text-6xl font-bold text-slate-600">Ethan Koran</h1>
-            <p className="text-2xl sm:text-3xl mt-4 text-slate-600 font-medium">
+          <div className="relative z-10 lg:mt-24">
+            <h1 className="text-5xl lg:text-6xl font-bold text-slate-600">Ethan Koran</h1>
+            <p className="text-2xl lg:text-3xl md:mt-4 mt-2 text-slate-600 font-medium">
               I'm a <span className="typed text-blue-500 font-medium z-10"></span><span className="typed-cursor" aria-hidden="true"></span>
             </p>
           </div>
@@ -96,13 +153,13 @@ export default function Home() {
         </section>
         {/* About Section */}
         <section id="about" className="min-h-screen bg-neutral-50 py-10">
-          <h2 className="text-3xl font-semibold text-center mb-10 text-slate-700">About Me</h2>
+          <h2 className="max-w-7xl pl-4 mx-auto text-3xl md:text-4xl text-center mb-10 font-bold text-neutral-700 dark:text-neutral-200 font-sans">About Me</h2>
           <div className="flex flex-col md:flex-row items-center justify-center px-6">
-            <div className="w-full md:w-1/2 mb-6 md:mb-0">
+            <div className="w-3/4 md:w-3/4 lg:w-1/2 mb-6 md:mb-0">
               <img 
                 src='/images/aboutmepic.jpeg'
                 alt="About Me"
-                className="w-full h-auto object-cover opacity-85"
+                className="w-auto h-auto object-cover opacity-85"
                 />
             </div>
             <div className="max-w-6xl mx-auto px-6">
@@ -115,20 +172,15 @@ export default function Home() {
 
         {/* Experience Section */}
         <section id="experience" className="min-h-screen bg-white py-20">
-          <div className="max-w-6xl mx-auto px-6">
-            <h2 className="text-3xl font-semibold text-center mb-10">My Experience</h2>
-            <ul className="space-y-6">
-              <li className="bg-gray-100 p-6 rounded-md shadow-md">
-                <h3 className="text-xl font-semibold">Job Title at Company 1</h3>
-                <p className="text-gray-700">Details about your role and achievements here.</p>
-              </li>
-              <li className="bg-gray-100 p-6 rounded-md shadow-md">
-                <h3 className="text-xl font-semibold">Job Title at Company 2</h3>
-                <p className="text-gray-700">Details about your role and achievements here.</p>
-              </li>
-              {/* Add more experience items as needed */}
-            </ul>
+          {/* <div className="max-w-6xl mx-auto px-6">
+            <h2 className="text-3xl font-semibold text-center">My Experience</h2> */}
+          <div className="lg:w-full h-full md:w-1/2 px-10">
+            <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+              My Experience
+            </h2>
+            {/* <Carousel items={cards}/>  */}
           </div>
+          {/* </div> */}
         </section>
 
         {/* Projects Section */}
